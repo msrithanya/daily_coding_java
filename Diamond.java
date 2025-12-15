a69b5c4311603a2d486853cfe059d390a3a893f3
Rithanya <rithanyamanoharan100@gmail.com>
2025-12-15 19:33:17 +0530

    first

---
+++ Diamond.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Diamond{
+    public static void main(String[] args){
+       Scanner s=new Scanner(System.in);
+       System.out.println("Enter any number");
+       int limit=s.nextInt();
+       for(int i=limit;i>=1;i--){
+        for(int j=1;j<=limit;j++){
+          if(j>=i){
+            System.out.print("* ");
+          }
+          else{
+             System.out.print(" ");
+          }
+        }
+            System.out.println("");
+       }
+       for(int i=1;i<=limit;i++){
+        for(int j=1;j<=limit;j++){
+          if(j>=i){
+            System.out.print("* ");
+          }
+          else{
+             System.out.print(" ");
+          }
+        }
+            System.out.println("");
+       }
+    }
+}
